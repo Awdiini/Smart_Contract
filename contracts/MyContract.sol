ed43eaa3266c3ee7e8488be10396f1966f7b437b
Awdiini <bashirawdini@gmail.com>
2022-07-05 23:55:28 +0300

    updates

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -7,17 +7,19 @@ contract MyContract {
     string internal Name = "Mobile";
     string public Receiver = "From me";
     uint public temp = 2;
+    uint256 public Arrival = block.timestamp;
 
-    function getFlower() external view returns(string memory, string memory, uint) {
-        return (Name,Receiver,temp);
+    function getFlower() external view returns(string memory, string memory, uint, uint256) {
+        return (Name,Receiver,temp, Arrival);
         
 
     }
 
-    function changeFlower(string memory _name, string memory _Rec, uint _t) external {
+    function changeFlower(string memory _name, string memory _Rec, uint _temp, uint256 _arrivalDate) external {
         Name = _name;
         Receiver = _Rec;
-        temp = _t;
+        temp = _temp;
+        Arrival = _arrivalDate;
 
     }
 }
